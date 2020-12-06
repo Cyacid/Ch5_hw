2b0ba9280b4e759e52cf92dad08713db03297943
wensen514 <wensen514@gmail.com>
2020-12-06 18:10:20 +0800

    First

---
+++ src/test_p10/pb/Sample3.java
@@ -0,0 +1,8 @@
+package test_p10.pb;
+
+public class Sample3 {
+	public static void main(String[] args){
+		test_p10.pc.Car car1 = new test_p10.pc.Car();
+		car1.show();
+	}
+}
---
+++ src/test_p6/Sample1.java
@@ -0,0 +1,9 @@
+package test_p6;
+
+class Sample1{
+	public static void main(String[] args){
+		Car car1;
+		car1 = new Car();
+		car1.show();
+	}
+}
